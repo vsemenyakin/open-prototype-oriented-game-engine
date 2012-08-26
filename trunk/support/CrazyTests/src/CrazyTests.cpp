25e04e1733c49c64593bb7ec7b5a20a7c930465a
semenyakinVSG@gmail.com <semenyakinVSG@gmail.com@91ac8a97-df74-c6c8-90c3-37e9fd683f85>
2012-08-26 03:42:56 +0000

    Added engine documentation

--- trunk/support/CrazyTests/src/CrazyTests.cpp
+++ trunk/support/CrazyTests/src/CrazyTests.cpp
@@ -5,11 +5,16 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
 #include <iostream>
+#include <string>
+#include "Base/PLObject.h"
+
 using namespace std;
 
+less<int>;
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	cout << "!!!Hello World!!!"<< endl; // prints !!!Hello World!!!
 	return 0;
 }
