cd9fd345a6ce0575ab8071723382bcae6f73796b
ShareDVI@gmail.com <ShareDVI@gmail.com@91ac8a97-df74-c6c8-90c3-37e9fd683f85>
2012-08-30 09:09:04 +0000

    Splitted PLLogFormatter in the new file

--- trunk/ open-prototype-oriented-game-engine --username semenyakinVSG@gmail.com/src/Base/Diagnostic/PLLog.cpp
+++ trunk/ open-prototype-oriented-game-engine --username semenyakinVSG@gmail.com/src/Base/Diagnostic/PLLog.cpp
@@ -1,44 +1,5 @@
  #include "PLLog.h"
 
-
-//PLLogFormatter methods.
-	const char* PLLogFormatter::get_datetime(const char* format)
-{
-	time_t rawtime;
-	struct tm* timeinfo;
-	static char buffer [100];
-	time ( &rawtime );
-	timeinfo = localtime ( &rawtime );
-	strftime(buffer, 100, format, timeinfo);
-	return buffer;
-}
-
-	const char* PLLogFormatter::format(const char *inMessage, const char* errorlevel, const char* channel=GENERAL)
-	{
-		static std::string buffer("");
-		buffer.clear();
-		if (_showLogLevel) {
-			buffer.append(_left_loglevel_bracket);
-			buffer.append(errorlevel);
-			buffer.append(_right_loglevel_bracket);
-		}
-
-		if (_showDatetime) {
-			buffer.append(_left_date_bracket);
-			buffer.append(get_datetime(_datetime_format));
-			buffer.append(_right_date_bracket);
-		}
-
-		if (_showChannel) {
-			buffer.append(_left_channel_bracket);
-			buffer.append(channel);
-			buffer.append(_right_channel_bracket);
-		}
-		buffer.append(inMessage);
-		return buffer.c_str();
-	}
-
-
 	//PLLog methods
 ////////////////////////////////////////////////////////
 PLLog::PLLog()
--- trunk/ open-prototype-oriented-game-engine --username semenyakinVSG@gmail.com/src/Base/Diagnostic/PLLog.h
+++ trunk/ open-prototype-oriented-game-engine --username semenyakinVSG@gmail.com/src/Base/Diagnostic/PLLog.h
@@ -5,7 +5,7 @@
 #include "../PLAutoPointer.h"
 #include <fstream>
 #include "../PLString.h"
-#include <time.h>
+#include "PLLogFormatter.h"
 
 //LOG LEVELS
 #define DEBUG "Debug"
@@ -18,46 +18,6 @@
 
 
 using namespace std;
-
-class PLLogFormatter {
-public:
-PLLogFormatter( // the HORRIBLE CONSTRUCTOR NEEDS TO be rewritten  using templates
-		const bool showLogLevel = true,
-		const bool showDatetime = true,
-		const bool showChannel = true,
-		const char* datetime_format = "%x %X",
-		const char* left_loglevel_bracket = "[",
-		const char* right_loglevel_bracket = "] ",
-		const char* left_date_bracket = "[",
-		const char* right_date_bracket = "] ",
-		const char* left_channel_bracket = "[",
-		const char* right_channel_bracket = "] "
-		) : _showLogLevel(showLogLevel), _showDatetime(showDatetime), _showChannel(showChannel), _datetime_format(datetime_format),
-				_left_loglevel_bracket(left_loglevel_bracket), _right_loglevel_bracket(right_loglevel_bracket),
-				_left_date_bracket(left_date_bracket), _right_date_bracket(right_date_bracket),
-				_left_channel_bracket(left_channel_bracket), _right_channel_bracket(right_channel_bracket) {}
-
-
-~PLLogFormatter() {}
-const char* format(const char *inMessage, const char* errorlevel, const char* channel);
-
-private:
-const char* get_datetime(const char* format);
-
-//fields
-const bool _showLogLevel;
-const bool _showDatetime;
-const bool _showChannel;
-const char* _datetime_format;
-
-const char* _left_loglevel_bracket;
-const char* _right_loglevel_bracket;
-const char* _left_date_bracket;
-const char* _right_date_bracket;
-const char* _left_channel_bracket;
-const char* _right_channel_bracket;
-
-};
 
 class PLLog {
 private:
