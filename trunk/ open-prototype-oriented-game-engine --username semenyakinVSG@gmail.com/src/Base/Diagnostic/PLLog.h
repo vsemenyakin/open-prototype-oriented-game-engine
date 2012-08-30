4b25e4fb638c3b2e21c8a73a9e64e6898d215920
ShareDVI@gmail.com <ShareDVI@gmail.com@91ac8a97-df74-c6c8-90c3-37e9fd683f85>
2012-08-30 18:54:08 +0000

    PLLog now store its channels&loglevels. Added abstract class IPLLogReceiver.

--- trunk/ open-prototype-oriented-game-engine --username semenyakinVSG@gmail.com/src/Base/Diagnostic/PLLog.h
+++ trunk/ open-prototype-oriented-game-engine --username semenyakinVSG@gmail.com/src/Base/Diagnostic/PLLog.h
@@ -2,9 +2,9 @@
 #define PLLOG_H_
 
 #include <iostream>
-#include "../PLAutoPointer.h"
 #include <fstream>
-#include "../PLString.h"
+#include <set>
+#include "IPLLogReceiver.h"
 #include "PLLogFormatter.h"
 
 //LOG LEVELS
@@ -19,20 +19,52 @@
 
 using namespace std;
 
-class PLLog {
+class PLLog : IPLLogReceiver {
 private:
 	std::ostream* stream_out;
 	PLLogFormatter* _formatter;
+	bool stream_responsibility;
+	bool formatter_responsibility;
 	bool isFile;
+	bool acceptsAllLoglevels;
+	bool acceptsAllChannels;
+	set<const char*>* acceptableLoglevels;
+	set<const char*>* acceptableChannels;
+	void init(const char* filename, PLLogFormatter* formatter);
+	void init(ostream* filename, PLLogFormatter* formatter);
+	bool acceptsLoglevel(const char* errorlevel);
+	bool acceptsChannel(const char* channel);
 public:
-	PLLog();
-	PLLog(const char* filename);
-	PLLog(PLLogFormatter* formatter);
-	PLLog(const char* filename, PLLogFormatter* formatter);
+	//constructor logic moved to init()
+
+
+	PLLog(const char* filename = "", PLLogFormatter* formatter=0)
+	{
+		init(filename, formatter);
+	}
+
+	PLLog(PLString* filename, PLLogFormatter* formatter=0)
+	{
+		init(filename->getCString(), formatter);
+	}
+
+	PLLog(ostream* stream, PLLogFormatter* formatter=0)
+	{
+		init(stream, formatter);
+	}
+
 	virtual ~PLLog();
 	void print(const char *inMessage, const char* errorlevel, const char* channel);
 	void print(const PLString *inMessage, const char* errorlevel, const char* channel);
 	void print(const PLString inMessage, const char* errorlevel, const char* channel);
+	bool accepts(const char* errorlevel, const char* channel);
+	void addLoglevel(const char* errorlevel);
+	bool deleteLoglevel(const char* errorlevel);
+	void addChannel(const char* channel);
+	bool deleteChannel(const char* channel);
+	void wipeLogLevels();
+	void wipeChannels();
+
 };
 
 #endif /* PLLOG_H_ */
