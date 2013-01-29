43ba2ec8f5bb4fab2ea2fe706178bdf35d4c6ebc
semenyakinVSG@gmail.com <semenyakinVSG@gmail.com@91ac8a97-df74-c6c8-90c3-37e9fd683f85>
2013-01-29 21:33:43 +0000

    Updated CrazyTests main file. Commited by Vladimir Semenyakin.

--- trunk/support/CrazyTests/src/CrazyTests.cpp
+++ trunk/support/CrazyTests/src/CrazyTests.cpp
@@ -1,20 +1,88 @@
-//============================================================================
-// Name        : CrazyTests.cpp
-// Author      : semenyakinVS
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-#include <iostream>
-#include <string>
-#include "Base/PLObject.h"
+/*
+ *		This Code Was Created By Jeff Molofee 2000
+ *		A HUGE Thanks To Fredric Echols For Cleaning Up
+ *		And Optimizing The Base Code, Making It More Flexible!
+ *		If You've Found This Code Useful, Please Let Me Know.
+ *		Visit My Site At nehe.gamedev.net
+ */
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream.h>
 
-using namespace std;
+#include <time.h>
 
-less<int>;
+#include "cpp_FAQ/Types.h"
 
-int main() {
+///////////////////////////////////////////////////////////////////////////////
+template<typename ItemType> class A;
+template<typename ItemType> class B;
 
-	cout << "!!!Hello World!!!"<< endl; // prints !!!Hello World!!!
+///////////////////////////////////////////////////////////////////////////////
+template <typename ItemType>
+class A
+{
+private:
+	ItemType anArgument;
+
+public:
+	template<typename ItemType_1> void operator = (A<ItemType_1> &inArgument)
+	{
+		anArgument = static_cast<ItemType_1>(inArgument.getVariable());
+	}
+
+
+	virtual void setVariable(const ItemType &inType)
+	{
+		anArgument = inType;
+	}
+
+	virtual ItemType &getVariable()
+	{
+		return anArgument;
+	}
+};
+
+///////////////////////////////////////////////////////////////////////////////
+template <typename ItemType>
+class B : public A<ItemType>
+{
+};
+
+///////////////////////////////////////////////////////////////////////////////
+int main()
+{
+	CPPTypes::printTypesSizes();
+	//CPPTypes::printStandartConstants();
+
+	CPPMemory::memoryAllocationTest();
+
+	/*
+	A<int> first;
+	A<double> second;
+
+	second.setVariable(-0.1);
+
+	first = second;
+	*/
+
+	/*
+	void *first = new B<int>();
+	void *second1;
+
+	clock_t theClockTime = clock();
+
+	int size = 40000;
+	int i, j;
+
+	for (i = 0; i < size; ++i)
+	{
+		for (j = 0; j < size; ++j)
+		{
+			//second1 = (A<int> *)first;
+			second1 = first;
+		}
+	}
+
+	std::cout << "Time: " << clock() - theClockTime << std::endl;
+*/
 	return 0;
 }
