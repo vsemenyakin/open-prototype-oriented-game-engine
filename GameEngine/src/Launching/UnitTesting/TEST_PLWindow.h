9fca3779ff49be1b0213e4b379909338d78f1154
semenyakinVSG@gmail.com <semenyakinVSG@gmail.com@91ac8a97-df74-c6c8-90c3-37e9fd683f85>
2013-02-18 15:42:27 +0000

    Added initial implementation of unit test for window. Commited by Vladimir Semenyakin.

---
+++ GameEngine/src/Launching/UnitTesting/TEST_PLWindow.h
@@ -0,0 +1,27 @@
+/*
+ * TEST_PLWindow.h
+ *
+ *  Created on: Feb 13, 2013
+ *      Author: Admin
+ */
+///////////////////////////////////////////////////////////////////////////////
+#ifndef TEST_PLWINDOW_H_
+#define TEST_PLWINDOW_H_
+
+///////////////////////////////////////////////////////////////////////////////
+#include "IPLUnitTest.h"
+
+///////////////////////////////////////////////////////////////////////////////
+class TEST_PLWindow : public IPLUnitTest
+{
+private:
+
+
+public:
+	TEST_PLWindow();
+	virtual ~TEST_PLWindow();
+
+	virtual void performTest();
+};
+
+#endif /* TEST_PLWINDOW_H_ */
